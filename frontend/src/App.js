2c1625b488187208bcf92d3f14f581005a51c03b
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-09-19 22:05:23 +0530

    feat: add Dockerfiles and update package.json for auth, notification, and todo services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function App() {
+
+export default function App() {
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState('');
 
+
   useEffect(() => {
-    axios.get('http://localhost:5000/todos').then(res => setTodos(res.data));
+    axios.get('http://localhost:5000/todos').then(r => setTodos(r.data));
   }, []);
+
 
   const add = async () => {
     await axios.post('http://localhost:5000/todos', { title, username: 'kasun' });
-    const res = await axios.get('http://localhost:5000/todos');
-    setTodos(res.data);
+    const r = await axios.get('http://localhost:5000/todos');
+    setTodos(r.data);
     setTitle('');
-  };
+  }
+
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Todo App (Microservices)</h1>
+      <h1>Todo App (microservices)</h1>
       <input value={title} onChange={e => setTitle(e.target.value)} placeholder="new todo" />
       <button onClick={add}>Add</button>
       <ul>
         {todos.map(t => <li key={t.id}>{t.title} (by {t.username})</li>)}
       </ul>
     </div>
-  );
+  )
 }
-
-export default App;
