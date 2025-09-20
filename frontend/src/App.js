136238f6b330f89a34091b648a76828f292427f5
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2025-09-20 12:40:44 +0530

    feat: update Dockerfiles, add .dockerignore files, and configure docker-compose for microservices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-
-export default function App() {
+function App() {
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState('');
 
-
   useEffect(() => {
-    axios.get('http://localhost:5000/todos').then(r => setTodos(r.data));
+    axios.get('http://localhost:5000/todos').then(res => setTodos(res.data));
   }, []);
-
 
   const add = async () => {
     await axios.post('http://localhost:5000/todos', { title, username: 'kasun' });
-    const r = await axios.get('http://localhost:5000/todos');
-    setTodos(r.data);
+    const res = await axios.get('http://localhost:5000/todos');
+    setTodos(res.data);
     setTitle('');
-  }
-
+  };
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Todo App (microservices)</h1>
+      <h1>Todo App (Microservices)</h1>
       <input value={title} onChange={e => setTitle(e.target.value)} placeholder="new todo" />
       <button onClick={add}>Add</button>
       <ul>
         {todos.map(t => <li key={t.id}>{t.title} (by {t.username})</li>)}
       </ul>
     </div>
-  )
+  );
 }
+
+export default App;
